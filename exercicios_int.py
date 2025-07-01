26fb7f88f176d92116a450843785bb699604c8ca
pedro.vml <pedro.vml@proton.me>
2025-06-30 23:53:05 -0300

    exercicios do tipo int

---
+++ exercicios_int.py
@@ -0,0 +1,44 @@
+#  #### Inteiros (`int`)
+# Alguns exemplos de métodos e operações:
+# + (adição)
+# - (subtração)
+# * (multiplicação)
+# // (divisão inteira)
+# % (módulo - resto da divisão)
+
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+# valor_1 = int(input("Digite o primeiro número: "))
+# valor_2 = int(input("Digite o segundo número: "))
+
+# resultado = valor_1 + valor_2
+
+# print(f"O resultado é: {resultado}")
+
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# valor = int(input("Insira um valor: "))
+# resultado = valor % 5
+
+# print(f"O resultado é: {resultado}")
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# valor_1 = int(input("Digite o primeiro número: "))
+# valor_2 = int(input("Digite o segundo número: "))
+
+# resultado = valor_1 * valor_2
+
+# print(f"O resultado é: {resultado}")
+
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+# valor_1 = int(input("Digite um número inteiro: "))
+# valor_2 = int(input("Digite outro número inteiro: "))
+
+# resultado = valor_1 // valor_2
+
+# print(f"O resultado é: {resultado}")
+
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+# valor = int(input("Digite um valor: "))
+
+# resultado = valor ** 2
+
+# print(f"O resultado é: {resultado}")
