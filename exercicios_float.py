064ee296bd96d25bc30d9a9a8be18448df3cfa00
pedro.vml <pedro.vml@proton.me>
2025-07-01 00:04:38 -0300

    exercicios do tipo float

---
+++ exercicios_float.py
@@ -0,0 +1,50 @@
+# #### Números de Ponto Flutuante (`float`)
+# Alguns exemplos de métodos e operações:
+# + (adição)
+# - (subtração)
+# * (multiplicação)
+# / (divisão)
+# ** (potenciação)
+
+# 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+# valor_1 = float(input("Digite o primeiro valor: "))
+# valor_2 = float(input("Digite o segundo valor: "))
+
+# resultado = valor_1 + valor_2
+
+# print(f"O resultado é: {resultado}")
+
+# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+# valor_1 = float(input("Digite o primeiro valor: "))
+# valor_2 = float(input("Digite o segundo valor: "))
+
+# resultado = (valor_1 + valor_2) / 2
+
+# print(f"O resultado é: {resultado}")
+
+# 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+# valor_base = float(input("Insira o valor da base: "))
+# valor_expoente = float(input("Insira o valor do expoente: "))
+
+# resultado = valor_base ** valor_expoente
+
+# print(f"O resultado é: {resultado}")
+
+# 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+# Fórmula: °F = (°C * 1.8) + 32
+
+# valor_celsius = float(input("Insira a temperatura em graus Celsius: "))
+
+# valor_fahrenheit = (valor_celsius * 1.8) + 32
+
+# print(f"{valor_celsius} °C equivalem a {valor_fahrenheit} °F")
+
+# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+# Fórmula: A = π r²
+# import math
+
+# valor_raio = float(input("Insira o valor do raio: "))
+
+# valor_area = math.pi * valor_raio ** 2
+
+# print(f"O valor da área é: {round(valor_area, 2)}")
