e588cb1051c6e7118331f5b1a5ee92fab5cf433c
pedro.vml <pedro.vml@proton.me>
2025-07-01 13:35:25 -0300

    exercicios do tipo boolean

---
+++ exercicios_bool.py
@@ -0,0 +1,49 @@
+# #### Booleanos (`bool`)
+# Alguns exemplos de métodos e operações:
+
+# and (E lógico)
+# or (OU lógico)
+# not (NÃO lógico)
+# == (igualdade)
+# != (diferença)
+
+# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+# valor_true = True
+# valor_false = False
+
+# resultado = valor_true and valor_false
+
+# print(resultado)
+
+# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+# valor_true = True
+# valor_false = False
+
+# resultado = valor_true or valor_false
+
+# print(resultado)
+
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+# valor_true = True
+# valor_false = False
+
+# valor_true_invertido = not valor_true
+# valor_false_invertido = not valor_false
+
+# print(valor_true_invertido, valor_false_invertido)
+
+# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+# valor_1 = 100
+# valor_2 = 300
+
+# resultado = valor_1 == valor_2
+
+# print(resultado)
+
+# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+# valor_1 = 100
+# valor_2 = 300
+
+# resultado = valor_1 != valor_2
+
+# print(resultado)
